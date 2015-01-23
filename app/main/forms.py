782eac17064dfab80663c1448e4b1ecaf4579bd9
HusseinGalal <hussein.galal.ahmed.11@gmail.com>
2015-01-23 02:52:00 +0200

    more structure

--- app/main/forms.py
+++
@@ -1,8 +0,0 @@
-from flask.ext.wtf import Form
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import Required
-
-class LoginForm(Form):
-    username = StringField('Username', validators=[Required()])
-    password = PasswordField('Password', validators=[Required()])
-    login = SubmitField("Login")
